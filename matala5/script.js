0c80df8cca99f8faa8d8a846a2230ea918c8bd0a
taisiasokolovskaya <sokoltaia02@gmail.com>
2024-12-05 13:44:52 +0200

    Update script.js

--- matala5/script.js
+++ matala5/script.js
@@ -1,3 +1,13 @@
 document.querySelectorAll('a').forEach(link => {
   link.addEventListener('click', () => alert('Link clicked!'));
 });
+// Example: Change text color on button click
+function changeTextColor() {
+  document.querySelector('h1').style.color = 'red';
+}
+
+// Add a button to the HTML
+const button = document.createElement('button');
+button.innerText = 'Change Title Color';
+button.onclick = changeTextColor;
+document.body.appendChild(button);
