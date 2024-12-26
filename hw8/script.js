9e9d323982a941b0815975f57300961403caeacc
taisiasokolovskaya <sokoltaia02@gmail.com>
2024-12-26 13:27:53 +0200

    Create script.js

---
+++ hw8/script.js
@@ -0,0 +1,13 @@
+console.log("הסקריפט נטען בהצלחה!");
+
+function divClicked() {
+    
+    alert("לחצת על הכפתור!");
+
+      const dorothy = document.getElementById("dorothy");
+
+        dorothy.style.left = "300px";
+    dorothy.style.top = "400px";
+
+       console.log("דורותי הוזזה למיקום (300px, 400px)!");
+}
