3fc36b0f6b6c61851fffcbecfb49318a1d305b0a
taisiasokolovskaya <sokoltaia02@gmail.com>
2025-03-19 13:16:17 +0200

    Update game.js

--- matala/JS/game.js
+++ matala/JS/game.js
@@ -6,24 +6,7 @@
     let positionX = window.innerWidth / 2;
     let positionY = window.innerHeight / 2;
 
-    function moveCharacter(targetX, targetY, callback) {
-        let interval = setInterval(() => {
-            if (Math.abs(positionX - targetX) > 5) {
-                positionX += positionX < targetX ? 5 : -5;
-            }
-            if (Math.abs(positionY - targetY) > 5) {
-                positionY += positionY < targetY ? 5 : -5;
-            }
-            
-            character.style.left = positionX + 'px';
-            character.style.top = positionY + 'px';
 
-            if (Math.abs(positionX - targetX) <= 5 && Math.abs(positionY - targetY) <= 5) {
-                clearInterval(interval);
-                if (callback) callback();
-            }
-        }, 50);
-    }
 const dorothy = document.querySelector('.character');
 
 // Function to move Dorothy to a new position
